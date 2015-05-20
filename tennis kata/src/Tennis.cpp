15290146cfa31b1e222b50bf8dc6ef899eddb1a1
Antonin Haas <tondahaas@gmail.com>
2015-05-20 18:22:35 +0200

    inicialized Game class with inicialization unit test

---
+++ tennis kata/src/Tennis.cpp
@@ -0,0 +1,8 @@
+#include "Tennis.h"
+using namespace std;
+Game::Game() { }
+
+string Game::GetScore()
+{
+	return "Love-Love";
+}
---
+++ tennis kata/src/Tennis.h
@@ -0,0 +1,11 @@
+#ifndef TENNIS_H__
+#define TENNIS_H__
+#include <string>
+class Game
+{
+	public:
+		Game(void);
+		std::string GetScore(void);
+};
+
+#endif
