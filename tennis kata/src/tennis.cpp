eec7aa0f7c26c0e1a0794cf80d817c350449ad37
Antonin Haas <tondahaas@gmail.com>
2015-05-20 23:13:10 +0200

    output for standart running score and for deuce

--- tennis kata/src/tennis.cpp
+++ tennis kata/src/tennis.cpp
@@ -4,5 +4,20 @@ Game::Game() { }
 
 string Game::GetScore()
 {
-	return "Love-Love";
+	if (scoreP1 == scoreP2 && scoreP1 >= 3)
+		return "Deuce";
+	string strP1, strP2;
+	for (auto entry : dict) {
+		if (scoreP1 == entry.first)
+			strP1 = entry.second;
+		if (scoreP2 == entry.first)
+			strP2 = entry.second;
+	}
+	return strP1 + "-" + strP2;
+}
+
+void Game::InitializeScore(int p1, int p2)
+{
+	scoreP1 = p1;
+	scoreP2 = p2;
 }
