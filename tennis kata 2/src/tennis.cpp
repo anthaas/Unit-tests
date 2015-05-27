6031b2a8175b99243d38310f5f77feb29ded88e7
Antonin Haas <tondahaas@gmail.com>
2015-05-28 00:41:27 +0200

    implementation of deuce

--- tennis kata 2/src/tennis.cpp
+++ tennis kata 2/src/tennis.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 string ScoreBoard::GetScore(int scorePlayer1, int scorePlayer2)
 {
+	if (scorePlayer2 >= 3 && scorePlayer1 == scorePlayer2)
+		return "Deuce";
 	return textrepre[scorePlayer1] + ":" + textrepre[scorePlayer2];
 
 }
